b2419b923f3c3ce3a2a437b0c4d6e676f832813a
Nicolas Diz <diz.nicolasamor@gmail.com>
2024-01-02 12:08:16 -0300

    MASTER: update createRoot import

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { createRoot } from 'react-dom/client';
+import { createRoot } from 'react-dom';
 import GameContainer from './game/GameContainer';
 
 const container = document.getElementById('root');
